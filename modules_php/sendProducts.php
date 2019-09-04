28c7d5d4866a46e80189bbcad242d857c004477d
codes_agency <andrenimacion@gmail.com>
2019-09-04 05:22:04 -0500

    correccion de error 2 envio de datos 2

--- modules_php/sendProducts.php
+++ modules_php/sendProducts.php
@@ -7,13 +7,13 @@
     $gImagen = addslashes(file_get_contents($_FILES['gImagen']['tmp_name']));
     $gPrecio = $_REQUEST['gPrecio'];
     $gStock = $_REQUEST['gStock'];
-/*
+
     echo $gName;
     echo $gDatos;
     echo $gPrecio;
     echo $gStock;
-*/
-    $queryInsert = "INSERT INTO gProducts(openDoor(),'gName')VALUE('$gName')" or die("Problemas en el select".mysqli_error(openDoor()));
-    header('location: error.php');
-    mysqli_close(openDoor());
+
+    $queryInsert = "INSERT INTO gProducts($cn,'gName')VALUE('$gName')" or die("Problemas en el select".mysqli_error($cn));
+    //header('location: error.php');
+    mysqli_close($cn);
 ?>
