7aaa21e3152d7a8c08da47943bba12077172fca9
akali <andrenimacion@gmail.com>
2019-10-01 10:54:01 -0500

    se esta trabajando en la mision y vision de la empresa

--- modules_php/body.php
+++ modules_php/body.php
@@ -3,9 +3,16 @@
     <p class="h3"><strong>Nuestra Empresa:</strong></p>
 </div>
 <div class="container mt-3 mb-3">
-   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-   Earum, minima. Atque deleniti officiis unde, cupiditate voluptate voluptates rerum! 
-   Ipsum, nisi. Excepturi illum autem doloribus dolores sapiente velit, rerum deserunt repudiandae.</p> 
+
+   <p style="color: black;">
+   <h2 style="color: steelblue"><strong> Global Services </strong></h2>
+   Estamos orientados a brindar un servicio de excelencia y calidad en la provisión 
+   de un servicio diferenciado de…………., con el mejor capital humano disponible en el
+    mercado laboral, ofreciéndoles a éstos la ventaja diferencial de un trabajo, 
+    involucra practica y capacitación constante, logrando así un profesional más 
+    identificado con nuestra empresa, nuestros clientes y en permanente desarrollo 
+    individual o personal.
+   </p> 
    
    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
    Earum, minima. Atque deleniti officiis unde, cupiditate voluptate voluptates rerum! 
--- modules_php/footer.php
+++ modules_php/footer.php
@@ -1,12 +1,29 @@
 <div class="container-fluid" id="footer">
-    <div class="box-f-1">
-        <img src="">
+    <div class="box-f-1" id="box-f-1" style="background-image: url('ul/footer.png');">
         <p>
-        <h4>Teléfonos:</h4>
-        <h5>242424242 - 0999599959</h5>
+           <div class="icos" id="icos-1">
+                <img src="ul/ico-01.png" width="35px" height="30px">
+                <p style="margin-top: 15px; margin-left: 7px;">@globalservices</p>
+           </div>
+           
+           <div class="icos" id="icos-1">
+                <img src="ul/mail-ico.png" width="35px" height="30px">
+                <p style="margin-top: 15px; margin-left: 7px;">ventas@globalservices.ec</p>
+           </div>
+
+           <div class="icos" id="icos-1">
+                <img src="ul/phone-ico.png" width="35px" height="30px">
+                <p style="margin-top: 15px; margin-left: 7px;">2472089 - 2753106 - 0987210849</p>
+           </div>
+           
+           <div class="icos" id="icos-1">
+                <img src="ul/site-ico.png" width="25px" height="30px">
+                <p style="margin-top: 15px; margin-left: 10px;">Dir.: Urb. Bellavista - Mz. 62 * Solar 29</p>
+           </div>
         </p>
     </div>
 
-    <div class="box-f-1">
+    <div class="box-f-1" id="box-f-2">
+        
     </div>
 </div>
